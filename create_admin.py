15715e800a5720f012d15e71702944ad2814e63c
mahfujars <mdmahfujarrahmannoyon09@gmail.com>
2026-05-01 11:43:04 +0600

    Remove create_admin.py setup script

--- create_admin.py
+++
@@ -1,9 +0,0 @@
-import os, django
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'oursolve_dashboard.settings')
-django.setup()
-from django.contrib.auth.models import User
-if not User.objects.filter(username='admin').exists():
-    User.objects.create_superuser('admin', 'admin@oursolve.com', 'Admin1234!')
-    print('Superuser created: admin / Admin1234!')
-else:
-    print('Admin user already exists')
